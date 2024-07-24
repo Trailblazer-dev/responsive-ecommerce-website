a43f029c8a76fc35aeb40ef48c9816326e4c3efb
Trailblazer-dev <richvictor830@gmail.com>
2024-07-24 13:20:01 +0300

    building

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,21 @@
 /*===== MENU SHOW =====*/ 
+const   navToggle = document.getElementById('nav-toggle');
 
+const showMenu = ()=>{
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('show');
+}
+navToggle.addEventListener('click', showMenu);
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction () {
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+// remove the show
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction)); 
